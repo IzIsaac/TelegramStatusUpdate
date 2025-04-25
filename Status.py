d91ddfb99497a8e660feeac90c36ff94b970b5db
IzIsaac <IsaacLamYJ@gmail.com>
2025-04-25 20:22:30 +0800

    Fixing reminder system

--- Status.py
+++ Status.py
@@ -854,9 +854,8 @@ async def start_scheduler():
     scheduler.add_job(run_timed_reminders, "cron", hour=8, misfire_grace_time=60)
     scheduler.add_job(run_timed_reminders, "cron", hour=12, misfire_grace_time=60)
     scheduler.add_job(run_timed_reminders, "cron", hour=18, misfire_grace_time=60)
-    scheduler.start()
 
-    scheduler.add_job(run_timed_reminders, "cron", hour=20, minute=20, misfire_grace_time=60)
+    scheduler.add_job(run_timed_reminders, "cron", hour=20, minute=25, misfire_grace_time=60)
     scheduler.start()
 
     # Ensure job is added before accessing it
